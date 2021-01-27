505584f597122fe3f4a969d1349c261402e257e8
Kehitt <kehitt@users.noreply.github.com>
2021-01-27 15:39:46 +0300

    Add AddExistingToken and CalculateTokenContext methods

--- proto/token_keeper.thrift
+++ proto/token_keeper.thrift
@@ -33,6 +33,10 @@ struct AuthData {
     5: required Metadata               metadata
 }
 
+struct TokenSourceContext {
+    1: required string request_origin
+}
+
 /**
  * Токен не может быть обработан.
  * Детали того, по каким причинам обработка невозможна, можно увидеть в аудит-логе. Причинами могут
@@ -47,6 +51,12 @@ exception InvalidToken {}
 
 exception AuthDataNotFound {}
 exception AuthDataRevoked {}
+
+/**
+ * Контекст токена не может быть вычислен
+ * Детали того, по каким причинам вычисление невозможно, можно увидеть в аудит-логе.
+ */
+exception ContextCreationFailed {}
 
 service TokenKeeper {
 
@@ -63,6 +73,14 @@ service TokenKeeper {
     * время жизни токена.
     **/
     AuthData CreateEphemeral (1: ContextFragment context, 2: Metadata metadata)
+
+    /**
+    * Добавить существующий токен
+    **/
+    AuthData AddExistingToken (1: Token token, 2: ContextFragment context, 3: Metadata metadata)
+        throws (
+            1: InvalidToken ex1
+    )
 
     /**
     * Получить данные токена по токену.
@@ -88,6 +106,19 @@ service TokenKeeper {
     void Revoke (1: AuthDataID id)
         throws (
             1: AuthDataNotFound ex1
+    )
+
+    /**
+    * Вычислить ContextFragment для токена, используя данные об обстоятельствах, при которых он поступил в обработку
+    *
+    * Данный метод не изменяет состояние и не гарантирует 100% корректности полученного фрагмента. Корректность
+    * должна быть подтверждена внешним потребителем, к примеру, успешностью полной авторизации операции с использованием
+    * этого фрагмента, а сам токен может быть сохранен используя метод AddExistingToken.
+    **/
+    ContextFragment CalculateTokenContext (1: Token token, 2: TokenSourceContext source_context)
+        throws (
+            1: InvalidToken ex1
+            2: ContextCreationFailed ex2
     )
 
 }
